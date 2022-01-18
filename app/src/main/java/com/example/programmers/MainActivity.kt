bf2c09e8a63cdca588ce48f9f010a1a20bf01999
pinocchio22 <vviian.2@gmail.com>
2022-01-18 17:25:26 +0900

    자연수 뒤집어 배열로 만들기

--- app/src/main/java/com/example/programmers/MainActivity.kt
+++ app/src/main/java/com/example/programmers/MainActivity.kt
@@ -11,17 +11,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        solution("2a34")
+        solution("try hello world")
     }
 
-    fun solution(s: String): Boolean {
-        var answer = true
-        if (s.length == 4 || s.length == 6) {
-            for (item in s) if (item.isDigit()) answer = true else { answer = false
-                    break }
-        } else answer = false
+    //이상한 문자 만들기
+    fun solution(s: String): String {
+        var answer = ""
+        println(s.split(" "))
         return answer
     }
+
+//    //자연수 뒤집어 배열로 만들기
+//    fun solution(n: Long): IntArray {
+//        var answer = intArrayOf()
+//        n.toString().map { answer += it.toInt() - 48 }
+//        return answer.reversedArray()
+//    }
+
+//    //정수 내림차순으로 배치하기
+//    fun solution(n: Long): Long {
+//        var answer: Long = 0
+//        answer = String(n.toString().toCharArray().sortedArrayDescending()).toLong()
+//        return answer
+//    }
+
+//    //정수의 제곱근 찾기
+//    fun solution(n: Long): Long {
+//        var answer: Long = 0
+//        for (x in 1.. n ) {
+//            if (n/x == x && n%x == 0L) {
+//                answer = (x+1)*(x+1)
+//                break
+//            } else answer = -1
+//        }
+//        println(answer)
+//        return answer
+//    }
+
+//    fun solution(s: String): Boolean {
+//        var answer = true
+//        if (s.length == 4 || s.length == 6) {
+//            for (item in s) if (item.isDigit()) answer = true else { answer = false
+//                    break }
+//        } else answer = false
+//        return answer
+//    }
 
 //    fun solution(n: Int): Int {
 //        var answer = 0
