3c3712944f09edcf028e6b92891f1fd8e355b91a
pinocchio22 <vviian.2@gmail.com>
2022-07-29 17:47:30 +0900

    순위 검색

--- app/src/main/java/com/example/programmers/MainActivity.kt
+++ app/src/main/java/com/example/programmers/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         arrayOf("java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"))
     }
 
+
     // 순위 검색
     fun solution(info:Array<String>, query:Array<String>):IntArray {
         val answer = IntArray(query.size) { 0 }
