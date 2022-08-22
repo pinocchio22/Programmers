25b5b7e480c01b37adf2fceabe3f52b5cd771a2d
pinocchio22 <vviian.2@gmail.com>
2022-08-22 22:06:22 +0900

    성격 유형 검사하기

--- app/src/main/java/com/example/programmers/MainActivity.kt
+++ app/src/main/java/com/example/programmers/MainActivity.kt
@@ -16,83 +16,104 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 7
-        solution(arrayOf(
-            "java backend junior pizza 150",
-            "python frontend senior chicken 210",
-            "python frontend senior chicken 150",
-            "cpp backend senior pizza 260",
-            "java backend junior chicken 80",
-            "python backend senior chicken 50"),
-        arrayOf("java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"))
+        // "TCMA"
+        solution(arrayOf("AN", "CF", "MJ", "RT", "NA"), intArrayOf(5, 3, 2, 7, 5))
     }
+    // 성격 유형 검사하기
+    fun solution(survey: Array<String>, choices: IntArray): String {
+        var answer: String = ""
+        val map =  mutableMapOf('R' to 0, 'T' to 0, 'C' to 0, 'F' to 0, 'J' to 0, 'M' to 0, 'A' to 0, 'N' to 0)
 
-
-    // 순위 검색
-    fun solution(info:Array<String>, query:Array<String>):IntArray {
-        val answer = IntArray(query.size) { 0 }
-        val wordMap:MutableMap<String, Int> = SettingWordMap()
-        val scoreList:MutableList<MutableList<Int>> = MutableList(4*3*3*3) { mutableListOf() }
-        val infoRegex = " ".toRegex()
-        val queryRegex = "( and )|( )".toRegex()
-
-        info.forEach {
-            val (infoList, score) = SplitInfo(it, infoRegex)
-            val arr:IntArray = intArrayOf(wordMap[infoList[0]]!! * 3 * 3 * 3, wordMap[infoList[1]]!! * 3 * 3, wordMap[infoList[2]]!! * 3, wordMap[infoList[3]]!!)
-            for(i in  0 until (1 shl 4)) {
-                var index = 0
-                for(j in 0 until 4) {
-                    if(i and (1 shl j) != 0) {
-                        index += arr[j]
-                    }
+        for (i in survey.indices) {
+            when {
+                choices[i] < 4 -> {
+                    // 왼쪽
+                    println(map[survey[i][0]])
+                    println(choices[i])
+                    map += survey[i][0] to 4 - choices[i]
                 }
-                scoreList[index].add(score)
-            }
-        }
-        scoreList.forEach {
-            it.sort()
-        }
-        for(i in query.indices) {
-            val(queryInfo, score) = SplitInfo(query[i], queryRegex)
-            val index:Int = wordMap[queryInfo[0]]!! * 3 * 3 * 3 + wordMap[queryInfo[1]]!! * 3 * 3 + wordMap[queryInfo[2]]!! * 3 + wordMap[queryInfo[3]]!!
-            var ret:Int = scoreList[index].binarySearch(score)
-            if (ret < 0) {
-                ret = (ret + 1) * -1
-            } else if(ret > 0) {
-                for(j in ret downTo 0) {
-                    if(scoreList[index][j] == score) {
-                        ret = j
-                    } else {
-                        break
-                    }
+                choices[i] > 4 -> {
+                    // 오른쪽
+                    println(map[survey[i][1]])
+                    println(choices[i]-4)
+                    map += survey[i][1] to choices[i]-4
+                }
+                else -> {
+                    // 0
+                    continue
                 }
             }
-            answer[i] += (scoreList[index].size - ret)
+            println(map)
         }
+
         return answer
     }
 
-    fun SettingWordMap():MutableMap<String, Int> {
-        val wordMap:MutableMap<String, Int> = HashMap<String, Int>()
-        wordMap["-"] = 0
-        wordMap["cpp"] = 1
-        wordMap["java"] = 2
-        wordMap["python"] = 3
-        wordMap["backend"] = 1
-        wordMap["frontend"] = 2
-        wordMap["junior"] = 1
-        wordMap["senior"] = 2
-        wordMap["chicken"] = 1
-        wordMap["pizza"] = 2
-
-        return wordMap
-    }
-
-    fun SplitInfo(info:String, regex:Regex):Pair<List<String>, Int> {
-        val infoList:List<String> = info.split(regex)
-        val score = infoList[4].toInt()
-        return Pair(infoList.subList(0, 4), score)
-    }
+//    // 순위 검색
+//    fun solution(info:Array<String>, query:Array<String>):IntArray {
+//        val answer = IntArray(query.size) { 0 }
+//        val wordMap:MutableMap<String, Int> = SettingWordMap()
+//        val scoreList:MutableList<MutableList<Int>> = MutableList(4*3*3*3) { mutableListOf() }
+//        val infoRegex = " ".toRegex()
+//        val queryRegex = "( and )|( )".toRegex()
+//
+//        info.forEach {
+//            val (infoList, score) = SplitInfo(it, infoRegex)
+//            val arr:IntArray = intArrayOf(wordMap[infoList[0]]!! * 3 * 3 * 3, wordMap[infoList[1]]!! * 3 * 3, wordMap[infoList[2]]!! * 3, wordMap[infoList[3]]!!)
+//            for(i in  0 until (1 shl 4)) {
+//                var index = 0
+//                for(j in 0 until 4) {
+//                    if(i and (1 shl j) != 0) {
+//                        index += arr[j]
+//                    }
+//                }
+//                scoreList[index].add(score)
+//            }
+//        }
+//        scoreList.forEach {
+//            it.sort()
+//        }
+//        for(i in query.indices) {
+//            val(queryInfo, score) = SplitInfo(query[i], queryRegex)
+//            val index:Int = wordMap[queryInfo[0]]!! * 3 * 3 * 3 + wordMap[queryInfo[1]]!! * 3 * 3 + wordMap[queryInfo[2]]!! * 3 + wordMap[queryInfo[3]]!!
+//            var ret:Int = scoreList[index].binarySearch(score)
+//            if (ret < 0) {
+//                ret = (ret + 1) * -1
+//            } else if(ret > 0) {
+//                for(j in ret downTo 0) {
+//                    if(scoreList[index][j] == score) {
+//                        ret = j
+//                    } else {
+//                        break
+//                    }
+//                }
+//            }
+//            answer[i] += (scoreList[index].size - ret)
+//        }
+//        return answer
+//    }
+//
+//    fun SettingWordMap():MutableMap<String, Int> {
+//        val wordMap:MutableMap<String, Int> = HashMap<String, Int>()
+//        wordMap["-"] = 0
+//        wordMap["cpp"] = 1
+//        wordMap["java"] = 2
+//        wordMap["python"] = 3
+//        wordMap["backend"] = 1
+//        wordMap["frontend"] = 2
+//        wordMap["junior"] = 1
+//        wordMap["senior"] = 2
+//        wordMap["chicken"] = 1
+//        wordMap["pizza"] = 2
+//
+//        return wordMap
+//    }
+//
+//    fun SplitInfo(info:String, regex:Regex):Pair<List<String>, Int> {
+//        val infoList:List<String> = info.split(regex)
+//        val score = infoList[4].toInt()
+//        return Pair(infoList.subList(0, 4), score)
+//    }
 
 //    // 문자열 압축
 //    fun solution(s: String): Int {
