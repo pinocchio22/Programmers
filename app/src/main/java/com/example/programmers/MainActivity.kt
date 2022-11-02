f8dc4323c539e869f974968173f57173bfcd980c
pinocchio22 <vviian.2@gmail.com>
2022-11-02 16:12:25 +0900

    숫자 짝꿍

--- app/src/main/java/com/example/programmers/MainActivity.kt
+++ app/src/main/java/com/example/programmers/MainActivity.kt
@@ -26,9 +26,10 @@ class MainActivity : AppCompatActivity() {
     // 연속 부분 수열 합의 개수
     fun solution(elements: IntArray): Int {
         var answer: Int = 0
+        var new = elements + elements
+        val set = mutableSetOf<Int>()
 
         //1
-//        val set = mutableSetOf<Int>()
 //
 //        for(i in 1 .. elements.size) {
 //            val result = mutableListOf<List<Int>>()
@@ -40,17 +41,37 @@ class MainActivity : AppCompatActivity() {
 //        println(set)
 
         //2
-        for (i in elements.indices) {
-            println("1 : ${elements[i]}")
-        }
 
-        for (i in 0..elements.size-2) {
-            println("2 : ${elements[i] + elements[i+2]}")
-        }
+        println(new.contentToString())
+        new.slice(1..3)
 
-        for (i in 0..elements.size-3) {
-            println("3 : ${elements[i] + elements[i+2] + elements[i+3]}")
+//        for (i in new.indices) {
+////            println("1 : ${elements[i]}")
+//            set += new[i]
+//        }
+//
+//        for (i in 0..new.size-2) {
+////            println("2 : ${elements[i] + elements[i+1]}")
+//            println(new.slice(i..i+1))
+//            set += new[i] + new[i+1]
+//        }
+//
+//        for (i in 0..new.size-3) {
+////            println("3 : ${elements[i] + elements[i+1] + elements[i+2]}")
+//            set += new[i] + new[i+1] + new[i+2]
+//        }
+////        println(set)
+
+        for (x in 1..elements.size) {
+//            println("x : $x")
+            for (y in 0..new.size-(x+1)) {
+//                println("y : $y")
+                println(new.slice(y..y+x))
+                set += new.slice(y..y+x).sum()
+                if (set.size == )
+            }
         }
+        println("set : $set")
 
         return answer
     }
